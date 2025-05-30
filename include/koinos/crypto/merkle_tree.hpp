449f3523c512fb775c05deefa01241aa6163d5e4
Steve Gerbino <steve@gerbino.co>
2025-05-30 13:36:38 +0200

    Add a merkle_root function that avoids creating a tree structure

--- include/koinos/crypto/merkle_tree.hpp
+++ include/koinos/crypto/merkle_tree.hpp
@@ -9,7 +9,7 @@
 
 namespace koinos::crypto {
 
-template< class T, bool hashed = false >
+template< typename ValueType, bool hashed = false >
 class merkle_node final
 {
 public:
@@ -18,8 +18,8 @@ public:
       _right( nullptr )
   {}
 
-  merkle_node( const T& value ) noexcept
-    requires( std::is_same_v< T, std::span< const std::byte > > )
+  merkle_node( const ValueType& value ) noexcept
+    requires( std::is_same_v< ValueType, std::span< const std::byte > > )
       :
       _left( nullptr ),
       _right( nullptr )
@@ -27,8 +27,9 @@ public:
     _hash = crypto::hash( reinterpret_cast< const void* >( value.data() ), value.size() );
   }
 
-  merkle_node( const T& value ) noexcept
-    requires( std::is_same_v< std::bool_constant< hashed >, std::true_type > && std::is_same_v< T, crypto::digest > )
+  merkle_node( const ValueType& value ) noexcept
+    requires( std::is_same_v< std::bool_constant< hashed >, std::true_type >
+              && std::is_same_v< ValueType, crypto::digest > )
       :
       _left( nullptr ),
       _right( nullptr )
@@ -36,14 +37,15 @@ public:
     _hash = value;
   }
 
-  merkle_node( const T& value ) noexcept:
+  merkle_node( const ValueType& value ) noexcept:
       _left( nullptr ),
       _right( nullptr )
   {
     _hash = crypto::hash( value );
   }
 
-  merkle_node( std::unique_ptr< merkle_node< T, hashed > > l, std::unique_ptr< merkle_node< T, hashed > > r ) noexcept:
+  merkle_node( std::unique_ptr< merkle_node< ValueType, hashed > > l,
+               std::unique_ptr< merkle_node< ValueType, hashed > > r ) noexcept:
       _left( std::move( l ) ),
       _right( std::move( r ) )
   {
@@ -64,25 +66,25 @@ public:
     return _hash;
   }
 
-  const std::unique_ptr< merkle_node< T, hashed > >& left() const noexcept
+  const std::unique_ptr< merkle_node< ValueType, hashed > >& left() const noexcept
   {
     return _left;
   }
 
-  const std::unique_ptr< merkle_node< T, hashed > >& right() const noexcept
+  const std::unique_ptr< merkle_node< ValueType, hashed > >& right() const noexcept
   {
     return _right;
   }
 
 private:
-  std::unique_ptr< merkle_node< T, hashed > > _left, _right;
+  std::unique_ptr< merkle_node< ValueType, hashed > > _left, _right;
   digest _hash{};
 };
 
-template< class T, bool hashed = false >
+template< typename ValueType, bool hashed = false >
 class merkle_tree final
 {
-  using node_type = merkle_node< T, hashed >;
+  using node_type = merkle_node< ValueType, hashed >;
 
   merkle_tree( std::unique_ptr< node_type > root ) noexcept:
       _root( std::move( root ) )
@@ -95,12 +97,12 @@ public:
   merkle_tree& operator=( merkle_tree&& rhs ) noexcept      = default;
   ~merkle_tree() noexcept                                   = default;
 
-  template< typename R >
-    requires std::ranges::range< R >
-  static merkle_tree< T, hashed > create( R&& values ) noexcept
+  template< typename Range >
+    requires std::ranges::range< Range >
+  static merkle_tree< ValueType, hashed > create( Range&& values ) noexcept
   {
     if( !values.size() )
-      return merkle_tree< T, hashed >( std::make_unique< node_type >() );
+      return merkle_tree< ValueType, hashed >( std::make_unique< node_type >() );
 
     std::vector< std::unique_ptr< node_type > > nodes;
     nodes.reserve( values.size() );
@@ -132,7 +134,7 @@ public:
       count = next_index;
     }
 
-    return merkle_tree< T, hashed >( std::move( nodes.front() ) );
+    return merkle_tree< ValueType, hashed >( std::move( nodes.front() ) );
   }
 
   const std::unique_ptr< node_type >& root() const noexcept
@@ -143,5 +145,52 @@ public:
 private:
   std::unique_ptr< node_type > _root;
 };
+
+template< bool hashed = false, typename Range >
+  requires std::ranges::range< Range >
+static digest merkle_root( Range&& values ) noexcept
+{
+  if( !values.size() )
+    return digest{};
+
+  std::vector< digest > nodes;
+  nodes.reserve( values.size() );
+
+  for( const auto& value: values )
+  {
+    if constexpr( hashed && std::is_same_v< std::ranges::range_value_t< Range >, crypto::digest > )
+      nodes.emplace_back( value );
+    else if constexpr( std::is_same_v< std::ranges::range_value_t< Range >, std::span< const std::byte > > )
+      nodes.emplace_back( hash( reinterpret_cast< const void* >( value.data() ), value.size() ) );
+    else
+      nodes.emplace_back( hash( value ) );
+  }
+
+  auto count = nodes.size();
+
+  while( count > 1 )
+  {
+    std::size_t next_index = 0;
+
+    for( std::size_t index = 0; index < count; index++ )
+    {
+      if( index + 1 < count )
+      {
+        hasher_reset();
+        hasher_update( nodes[ index++ ] );
+        hasher_update( nodes[ index ] );
+        nodes[ next_index++ ] = hasher_finalize();
+      }
+      else
+      {
+        nodes[ next_index++ ] = std::move( nodes[ index ] );
+      }
+    }
+
+    count = next_index;
+  }
+
+  return nodes.front();
+}
 
 } // namespace koinos::crypto
--- src/koinos/crypto/merkle_tree.test.cpp
+++ src/koinos/crypto/merkle_tree.test.cpp
@@ -23,6 +23,7 @@ TEST( merkle_root, root )
   auto tree1 = koinos::crypto::merkle_tree< std::string >::create( values1 );
   std::cout << hex_string( tree1.root()->hash() ) << std::endl;
   EXPECT_EQ( hex_string( tree1.root()->hash() ), "6bf65112983e69ab0960c2cfd637c0aca1fa071f8d1abc96135600d9180cee9a" );
+  EXPECT_EQ( tree1.root()->hash(), koinos::crypto::merkle_root( values1 ) );
 
   std::vector< koinos::crypto::digest > values2{ koinos::crypto::hash( "the" ),
                                                  koinos::crypto::hash( "quick" ),
@@ -36,6 +37,7 @@ TEST( merkle_root, root )
   auto tree2 = koinos::crypto::merkle_tree< koinos::crypto::digest, true >::create( values2 );
   std::cout << hex_string( tree2.root()->hash() ) << std::endl;
   EXPECT_EQ( hex_string( tree2.root()->hash() ), "df7ace9830eb1550840a96e8cdf5474f51c375d75de722161daa9ca4be5bb3eb" );
+  EXPECT_EQ( tree2.root()->hash(), koinos::crypto::merkle_root< true >( values2 ) );
 
   std::vector< std::span< const std::byte > > values3;
   for( const auto& value: values2 )
@@ -43,4 +45,5 @@ TEST( merkle_root, root )
   auto tree3 = koinos::crypto::merkle_tree< std::span< const std::byte > >::create( values3 );
   std::cout << hex_string( tree3.root()->hash() ) << std::endl;
   EXPECT_EQ( hex_string( tree3.root()->hash() ), "df78a7ede227b96b8b5b072da0d62da12307c7be6f064a4b08ba1aecd2d0d9d6" );
+  EXPECT_EQ( tree3.root()->hash(), koinos::crypto::merkle_root( values3 ) );
 }
