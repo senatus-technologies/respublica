92f02345ea0eb281d7f96ec9752b919886815afa
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2025-06-04 11:26:13 -0600

    Remove toolchains

--- tools/cmake/toolchains/clang-base.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER "clang")
-set(CMAKE_CXX_COMPILER "clang++")
-
-#set(CMAKE_CXX_FLAGS_INIT "--stdlib=libc++")
--- tools/cmake/toolchains/gcc-base.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER "gcc")
-set(CMAKE_CXX_COMPILER "g++")
-
-set(CMAKE_CXX_FLAGS_INIT "--stdlib=libstdc++")
