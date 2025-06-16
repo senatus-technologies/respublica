c01cd8595098526c90ba7464fb9c7ca21939bc59
Steve Gerbino <steve@gerbino.co>
2025-06-16 03:55:57 +0200

    Format fix project

--- include/koinos/state_db/state_db.hpp
+++ include/koinos/state_db/state_db.hpp
@@ -3,4 +3,3 @@
 #include <koinos/state_db/database.hpp>
 #include <koinos/state_db/state_node.hpp>
 #include <koinos/state_db/types.hpp>
-
--- src/koinos/vm_manager/fizzy/module_cache.hpp
+++ src/koinos/vm_manager/fizzy/module_cache.hpp
@@ -20,16 +20,17 @@ public:
   module_guard( const FizzyModule* m ):
       _module( m )
   {}
+
   module_guard( const module_guard& ) = delete;
-  module_guard( module_guard&& ) = delete;
+  module_guard( module_guard&& )      = delete;
 
   ~module_guard()
   {
     fizzy_free_module( _module );
   }
 
-  module_guard& operator =( const module_guard& ) = delete;
-  module_guard& operator =( module_guard&& ) = delete;
+  module_guard& operator=( const module_guard& ) = delete;
+  module_guard& operator=( module_guard&& )      = delete;
 
   const FizzyModule* get() const
   {
@@ -43,7 +44,8 @@ class module_cache
 {
 private:
   using lru_list_type = std::list< std::vector< std::byte > >;
-  using module_map_type = std::map< std::span< const std::byte >, std::pair< module_ptr, typename lru_list_type::iterator >, bytes_less >;
+  using module_map_type =
+    std::map< std::span< const std::byte >, std::pair< module_ptr, typename lru_list_type::iterator >, bytes_less >;
 
   lru_list_type _lru_list;
   module_map_type _module_map;
@@ -53,12 +55,12 @@ private:
 public:
   module_cache( std::size_t size = 32 );
   module_cache( const module_cache& ) = delete;
-  module_cache( module_cache&& ) = delete;
+  module_cache( module_cache&& )      = delete;
 
   ~module_cache();
 
-  module_cache& operator =( const module_cache& ) = delete;
-  module_cache& operator =( module_cache&& ) = delete;
+  module_cache& operator=( const module_cache& ) = delete;
+  module_cache& operator=( module_cache&& )      = delete;
 
   module_ptr get_module( std::span< const std::byte > id );
   void put_module( std::span< const std::byte > id, const module_ptr& module );
