4a74fdebfef3a0da922b671bcc14304ebb2a8e06
Steve Gerbino <steve@gerbino.co>
2025-06-27 01:31:29 +0200

    Break out program context from virtual machine code

--- src/koinos/vm/module_cache.hpp
+++ src/koinos/vm/module_cache.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <fizzy/fizzy.h>
 
 #include <algorithm>
---
+++ src/koinos/vm/program_context.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <fizzy/fizzy.h>
+
+#include <koinos/vm/host_api.hpp>
+#include <koinos/vm/module_cache.hpp>
+
+namespace koinos::vm {
+
+class program_context
+{
+public:
+  program_context()                         = delete;
+  program_context( const program_context& ) = delete;
+  program_context( program_context&& )      = delete;
+  program_context( host_api& h, const module_ptr& m ) noexcept;
+
+  ~program_context();
+
+  program_context& operator=( const program_context& ) = delete;
+  program_context& operator=( program_context&& )      = delete;
+
+  std::error_code start() noexcept;
+
+  FizzyExecutionResult _wasi_args_get( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_args_sizes_get( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_fd_seek( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_fd_write( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_fd_read( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_fd_close( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_fd_fdstat_get( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _wasi_proc_exit( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+
+  FizzyExecutionResult _koinos_get_caller( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _koinos_get_object( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _koinos_put_object( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _koinos_check_authority( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _koinos_log( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+  FizzyExecutionResult _koinos_exit( const FizzyValue* args, FizzyExecutionContext* fizzy_context ) noexcept;
+
+private:
+  host_api* _hapi                       = nullptr;
+  module_ptr _module                    = nullptr;
+  FizzyInstance* _instance              = nullptr;
+  FizzyExecutionContext* _fizzy_context = nullptr;
+  std::int32_t _exit_code               = 0;
+
+  std::error_code instantiate_module() noexcept;
+};
+
+} // namespace koinos::vm
