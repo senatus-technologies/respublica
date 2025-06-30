4c5cac1fa9669175b223b330671c149441da5790
Steve Gerbino <steve@gerbino.co>
2025-06-30 03:52:57 +0200

    Cleaned up token implementation

--- tools/wasm/tests/token.c
+++ tools/wasm/tests/token.c
@@ -17,11 +17,11 @@ extern int32_t
 koinos_put_object( uint32_t id, const char* key_ptr, uint32_t key_len, const char* value_ptr, uint32_t value_len );
 extern int32_t koinos_check_authority( const char* account_ptr, uint32_t account_len, bool* value );
 
-bool check_authority( account_t acc )
+static inline bool check_authority( account_t account )
 {
   bool authorized = false;
 
-  int32_t code = koinos_check_authority( acc, ACCOUNT_LENGTH, &authorized );
+  int32_t code = koinos_check_authority( account, ACCOUNT_LENGTH, &authorized );
 
   if( code )
     exit( code );
@@ -40,26 +40,27 @@ const size_t supply_key_size  = 0;
 
 enum instructions
 {
-  authorize_instr,
-  name_instr,
-  symbol_instr,
-  decimals_instr,
-  total_supply_instr,
-  balance_of_instr,
-  transfer_instr,
-  mint_instr,
-  burn_instr
+  authorize_instruction,
+  name_instruction,
+  symbol_instruction,
+  decimals_instruction,
+  total_supply_instruction,
+  balance_of_instruction,
+  transfer_instruction,
+  mint_instruction,
+  burn_instruction
 };
 
 enum errc
 {
-  errc_ok                   = 0,
-  errc_unauthorized         = 1,
-  errc_insufficient_balance = 2,
-  errc_insufficient_supply  = 3,
-  errc_invalid_argument     = 4,
-  errc_invalid_object       = 5,
-  errc_overflow             = 6
+  ok,
+  unauthorized,
+  invalid_instruction,
+  insufficient_balance,
+  insufficient_supply,
+  invalid_argument,
+  unexpected_object,
+  overflow
 };
 
 uint64_t total_supply()
@@ -74,7 +75,7 @@ uint64_t total_supply()
     return 0;
 
   if( num_bytes != sizeof( uint64_t ) )
-    exit( errc_invalid_object );
+    exit( unexpected_object );
 
   return supply;
 }
@@ -90,46 +91,46 @@ uint64_t balance_of( account_t account )
     return 0;
 
   if( num_bytes != sizeof( uint64_t ) )
-    exit( errc_invalid_object );
+    exit( unexpected_object );
 
   return balance;
 }
 
 int main( void )
 {
-  uint32_t entry_point;
-  read( STDIN_FILENO, &entry_point, sizeof( uint32_t ) );
+  uint32_t instruction;
+  read( STDIN_FILENO, &instruction, sizeof( uint32_t ) );
 
-  switch( entry_point )
+  switch( instruction )
   {
-    case authorize_instr:
+    case authorize_instruction:
       {
         const bool no = false;
         write( STDOUT_FILENO, &no, sizeof( bool ) );
         break;
       }
-    case name_instr:
+    case name_instruction:
       {
         write( STDOUT_FILENO, token_name, strlen( token_name ) );
         break;
       }
-    case symbol_instr:
+    case symbol_instruction:
       {
         write( STDOUT_FILENO, token_symbol, strlen( token_symbol ) );
         break;
       }
-    case decimals_instr:
+    case decimals_instruction:
       {
         write( STDOUT_FILENO, &token_decimals, sizeof( token_decimals ) );
         break;
       }
-    case total_supply_instr:
+    case total_supply_instruction:
       {
         uint64_t supply = total_supply();
         write( STDOUT_FILENO, &supply, sizeof( uint64_t ) );
         break;
       }
-    case balance_of_instr:
+    case balance_of_instruction:
       {
         account_t account;
 
@@ -139,7 +140,7 @@ int main( void )
         write( STDOUT_FILENO, &balance, sizeof( uint64_t ) );
         break;
       }
-    case transfer_instr:
+    case transfer_instruction:
       {
         account_t from;
         account_t to;
@@ -150,7 +151,7 @@ int main( void )
         read( STDIN_FILENO, &value, sizeof( uint64_t ) );
 
         if( !memcmp( from, to, ACCOUNT_LENGTH ) )
-          exit( errc_invalid_argument );
+          exit( invalid_argument );
 
         account_t caller;
         uint32_t num_bytes = ACCOUNT_LENGTH;
@@ -161,21 +162,21 @@ int main( void )
         if( num_bytes )
         {
           if( num_bytes != ACCOUNT_LENGTH )
-            exit( errc_invalid_object );
+            exit( unexpected_object );
 
           if( memcmp( caller, from, ACCOUNT_LENGTH ) && !check_authority( from ) )
-            exit( errc_unauthorized );
+            exit( unauthorized );
         }
         else
         {
           if( !check_authority( from ) )
-            exit( errc_unauthorized );
+            exit( unauthorized );
         }
 
         uint64_t from_balance = balance_of( from );
 
         if( from_balance < value )
-          exit( errc_insufficient_balance );
+          exit( insufficient_balance );
 
         uint64_t to_balance = balance_of( to );
 
@@ -187,7 +188,7 @@ int main( void )
 
         break;
       }
-    case mint_instr:
+    case mint_instruction:
       {
         account_t to;
         uint64_t value;
@@ -198,7 +199,7 @@ int main( void )
         uint64_t supply = total_supply();
 
         if( UINT64_MAX - value < supply )
-          exit( errc_overflow );
+          exit( overflow );
 
         uint64_t to_balance = balance_of( to );
 
@@ -209,7 +210,7 @@ int main( void )
         koinos_put_object( balance_id, to, ACCOUNT_LENGTH, (char*)&to_balance, sizeof( to_balance ) );
         break;
       }
-    case burn_instr:
+    case burn_instruction:
       {
         account_t from;
         uint64_t value;
@@ -226,25 +227,25 @@ int main( void )
         if( num_bytes )
         {
           if( num_bytes != ACCOUNT_LENGTH )
-            exit( errc_invalid_object );
+            exit( unexpected_object );
 
           if( memcmp( caller, from, ACCOUNT_LENGTH ) && !check_authority( from ) )
-            exit( errc_unauthorized );
+            exit( unauthorized );
         }
         else
         {
           if( !check_authority( from ) )
-            exit( errc_unauthorized );
+            exit( unauthorized );
         }
 
         uint64_t from_balance = balance_of( from );
         if( value > from_balance )
-          exit( errc_insufficient_balance );
+          exit( insufficient_balance );
 
         uint64_t supply = total_supply();
 
         if( value > supply )
-          exit( errc_insufficient_supply );
+          exit( insufficient_supply );
 
         supply       -= value;
         from_balance -= value;
@@ -255,9 +256,9 @@ int main( void )
       }
     default:
       {
-        exit( errc_invalid_argument );
+        exit( invalid_instruction );
       }
   }
 
-  return errc_ok;
+  return ok;
 }
