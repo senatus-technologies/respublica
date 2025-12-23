9a7e11efce373544752dcb8965afc9fdc49890c7
Steve Gerbino <steve@gerbino.co>
2025-12-23 16:17:06 +0100

    Add support for user supplied log sinks to the log module

--- include/respublica/log/log.hpp
+++ include/respublica/log/log.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include <quill/LogMacros.h>
+#include <quill/sinks/Sink.h>
 
 #include <respublica/log/formatter.hpp>
 #include <respublica/log/frontend.hpp>
 
 namespace respublica::log {
 
-void initialize() noexcept;
+void initialize( std::vector< std::shared_ptr< quill::Sink > > sinks = {} ) noexcept;
 logger* instance() noexcept;
 
 } // namespace respublica::log
--- src/respublica/log/log.cpp
+++ src/respublica/log/log.cpp
@@ -8,8 +8,13 @@
 
 namespace respublica::log {
 
-void initialize() noexcept
+static std::vector< std::shared_ptr< quill::Sink > >
+  configured_sinks; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+
+void initialize( std::vector< std::shared_ptr< quill::Sink > > sinks ) noexcept
 {
+  configured_sinks = std::move( sinks );
+
   constexpr auto sleep_duration = std::chrono::seconds{ 1 };
 
   quill::BackendOptions options;
@@ -24,14 +29,31 @@ void initialize() noexcept
 
 logger* instance() noexcept
 {
-  static auto logger = frontend::create_or_get_logger(
-    "root",
-    frontend::create_or_get_sink< quill::ConsoleSink >( "console_sink_id_1" ),
-    quill::PatternFormatterOptions{ "%(time) [%(thread_id)] %(short_source_location:<28) %(log_level_short_code:<2) "
-                                    "%(tags)%(message)",
-                                    "%Y-%m-%d %H:%M:%S.%Qms",
-                                    quill::Timezone::GmtTime } );
-  return logger;
+  static auto logger_instance = []() -> logger*
+  {
+    std::vector< std::shared_ptr< quill::Sink > > sinks;
+
+    if( !configured_sinks.empty() )
+    {
+      // Use user-provided sinks
+      sinks = configured_sinks;
+    }
+    else
+    {
+      // Create default console sink
+      sinks.push_back( frontend::create_or_get_sink< quill::ConsoleSink >( "console_sink_id_1" ) );
+    }
+
+    return frontend::create_or_get_logger(
+      "root",
+      sinks,
+      quill::PatternFormatterOptions{ "%(time) [%(thread_id)] %(short_source_location:<28) %(log_level_short_code:<2) "
+                                      "%(tags)%(message)",
+                                      "%Y-%m-%d %H:%M:%S.%Qms",
+                                      quill::Timezone::GmtTime } );
+  }();
+
+  return logger_instance;
 }
 
 } // namespace respublica::log
